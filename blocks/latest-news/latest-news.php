cb55dbe0eb7c5a67d41449b4aa4296b73188183c
William Kunitake <william@bulkdesign.com.br>
2024-02-11 20:52:52 -0800

    Included thumbnails to second column of posts

--- blocks/latest-news/latest-news.php
+++ blocks/latest-news/latest-news.php
@@ -10,7 +10,7 @@
 <section <?php theme_block_attributes( $block, 'latest-news' ); ?>>
 
     <?php if ( ! empty( get_field('title') ) ): ?>
-        <h1><?php echo get_field('title'); ?></h1>
+        <h2><?php echo get_field('title'); ?></h2>
     <?php endif; ?>
 
     <?php if ( ! empty( get_field('subtitle') ) ): ?>
@@ -57,7 +57,7 @@
         $list = array( 
             'post_type'      => array( 'post'),
             'post_status'    => 'publish',
-            'posts_per_page' => 3,
+            'posts_per_page' => 4,
             'orderby'        => 'date',
             'order'          => 'DESC',
             'offset'         => 1
@@ -72,7 +72,13 @@
                 <?php $the_query->the_post(); ?>
                 <li>
                     <a href="<?php echo get_permalink(); ?>">
-                        <h3><?php the_title(); ?></h3>
+                        <?php $featured_image = get_the_post_thumbnail_url( get_the_ID(), 'large' ); ?>
+                        <?php $featured_image_alt = get_post_meta($featured_image , '_wp_attachment_image_alt', true); ?>
+
+                        <?php if ( ! empty( $featured_image ) ) : ?>
+                            <img src="<?php echo $featured_image; ?>" alt="<?php echo $featured_image_alt; ?>" title="<?php the_title(); ?>">
+                            <h3><?php the_title(); ?></h3>
+                        <?php endif; ?>
                     </a>
                     <p class="excerpt">
                         <?php the_excerpt(); ?>
